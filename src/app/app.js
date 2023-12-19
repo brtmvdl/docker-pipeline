fa19583c2d7048bc8d64dd53c6fc8e8e3ad830c2
Tarsis <br.tmvdl@gmail.com>
2023-12-19 11:37:27 -0300

    Updating app files

---
+++ src/app/app.js
@@ -0,0 +1,7 @@
+import { Router } from '@brtmvdl/backend'
+
+const router = new Router()
+
+router.get('/', (_, res) => res)
+
+export default router
--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,23 @@
+import net from 'net'
 
+import { HttpRequest, HttpResponse } from '@brtmvdl/backend'
+
+import { Logger } from '@brtmvdl/logger'
+
+import { PORT } from './config.js'
+
+import app from './app.js'
+
+const logger = new Logger('INDEX')
+
+const server = net.createServer((socket) => {
+  socket.on('data', (data) => {
+    const req = new HttpRequest(data.toString())
+    const res = new HttpResponse(req)
+    const str = app.run(req, res).toString()
+    socket.write(str)
+    socket.end()
+  })
+})
+
+server.listen(PORT, () => logger.log(`Server runnig on ${PORT}.`))
